efc935f82e9322a63580e5d54ea8ff7d8f27697d
SheilaNS <shei.nsantos@gmail.com>
2022-05-31 15:00:31 -0300

    Conteúdo + Exercícios dia 21.1

---
+++ bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/conteudo/fixando-3.2.sql
@@ -0,0 +1,6 @@
+SELECT 15 AS Number,
+IF(15 MOD 2 = 0, 'Par', 'Ímpar') AS 'Par ou Ímpar?';
+
+SELECT 220 AS 'Capacidade da sala', 220 DIV 12 AS 'Grupos de 12 pessoas';
+
+SELECT 220 MOD 18 AS 'Lugarem sobrando';
