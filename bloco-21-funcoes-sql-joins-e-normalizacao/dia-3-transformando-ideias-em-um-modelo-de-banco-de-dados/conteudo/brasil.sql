f5c450366167e4de6fa6067cbb3a8cdafc5d7c7c
SheilaNS <shei.nsantos@gmail.com>
2022-06-01 19:26:59 -0300

    [FEAT] Conteúdo do dia 21.3

---
+++ bloco-21-funcoes-sql-joins-e-normalizacao/dia-3-transformando-ideias-em-um-modelo-de-banco-de-dados/conteudo/brasil.sql
@@ -0,0 +1,14 @@
+  DROP SCHEMA IF EXISTS Brasil;
+  CREATE SCHEMA Brasil;
+  USE Brasil;
+
+  CREATE TABLE cidades(
+    cidade VARCHAR(100) NOT NULL,
+    estado VARCHAR(10) NOT NULL,
+    populacao INTEGER,
+    CONSTRAINT PRIMARY KEY(cidade)
+  );
+  
+INSERT INTO cidades(cidade, estado, populacao)
+VALUES('Rio Claro','SP',185421),
+	  ('Rio Claro','RJ',17216);
