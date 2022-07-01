fa498801d6e8a63cc4509d482e00c4eb7c923538
SheilaNS <shei.nsantos@gmail.com>
2022-06-30 22:44:39 -0300

    [FEAT] Aula ao vivo terminada

--- bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/class-example-mysql/src/index.js
+++ bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/class-example-mysql/src/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const connection = require('../models/connections');
+const movies = require('../models/movies');
 
 const app = express();
 app.use(express.json());
@@ -7,19 +7,14 @@ app.use(express.json());
 const MOVIES_ID = '/movies/:id';
 const MOVIES = '/movies';
 
-const GET_SQL = 'select * from db.movies where id = ?;';
-
 const NAO_ENCONTRADO = { message: 'id não encontrado' };
 
 // removeMovie
 app.delete(MOVIES_ID, async (req, res) => {
   try {
     const { id } = req.params;
-    const sql = 'delete from db.movies where id = ?;';
-    const result = await connection.query(sql, [id]);
-    const status = result[0];
-    const { affectedRows } = status;
-    if (affectedRows > 0) return res.sendStatus(204);
+    const deleted = await movies.delMovie(id);
+    if (deleted) return res.sendStatus(204);
     res.status(404).json(NAO_ENCONTRADO);
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -32,15 +27,15 @@ app.put(MOVIES_ID, async (req, res) => {
     const { id } = req.params;
     const changes = req.body;
 
-    const [[oldItem]] = await connection.query(GET_SQL, [id]);
+    const oldItem = await movies.getMovie(id);
     if (!oldItem) return res.status(404).json(NAO_ENCONTRADO);
-    const updatedSql = 'update db.movies set ? where id = ?;';
-    const result = await connection.query(updatedSql, [changes, id]);
-    const status = result[0];
-    const { affectedRows } = status;
-    if (affectedRows === 0) return res.status(400).json({ message: 'Erro ao alterar' });
-    const [[newItem]] = await connection.query(GET_SQL, [id]);
+
+    const updated = await movies.editMovie(id, changes);
+    if (!updated) return res.status(400).json({ message: 'Erro ao alterar' });
+
+    const newItem = await movies.getMovie(id);
     if (!newItem) return res.status(404).json(NAO_ENCONTRADO);
+
     res.json(newItem);
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -51,8 +46,7 @@ app.put(MOVIES_ID, async (req, res) => {
 app.get(MOVIES_ID, async (req, res) => {
   try {
     const { id } = req.params;
-    const sql = 'select * from db.movies where id = ?;';
-    const [[item]] = await connection.query(sql, [id]);
+    const item = await movies.getMovie(id);
     if (!item) return res.status(404).json(NAO_ENCONTRADO);
     res.status(200).json(item);
   } catch (error) {
@@ -63,8 +57,7 @@ app.get(MOVIES_ID, async (req, res) => {
 // getAllMovies
 app.get(MOVIES, async (_req, res) => {
   try {
-    const sql = 'select * from db.movies;';
-    const [items] = await connection.query(sql);
+    const items = await movies.getMovie();
     res.status(200).json(items);
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -75,12 +68,8 @@ app.get(MOVIES, async (_req, res) => {
 app.post(MOVIES, async (req, res) => {
   try {
     const { name, year } = req.body;
-    const sql = 'insert into db.movies (name, year) values (?, ?);';
-    const result = await connection.query(sql, [name, year]);
-    const status = result[0];
-    const { insertId } = status;
-
-    const [[item]] = await connection.query(GET_SQL, [insertId]);
+    const inserted = await movies.addMovie(name, year);
+    const item = await movies.getMovie(inserted);
     res.json(item);
   } catch (error) {
     res.status(500).json({ message: error.message });
