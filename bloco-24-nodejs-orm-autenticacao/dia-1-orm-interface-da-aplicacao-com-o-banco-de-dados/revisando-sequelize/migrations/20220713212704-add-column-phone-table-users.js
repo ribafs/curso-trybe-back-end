a9fc1d49868a853bee2d3d1088d0fcbf32016c3c
SheilaNS <shei.nsantos@gmail.com>
2022-07-13 19:21:20 -0300

    feat(revisão): :construction: Revisando Sequelize
    Adicionando o passo a passo e revendo a configuração e conceitos de Sequelize

---
+++ bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/revisando-sequelize/migrations/20220713212704-add-column-phone-table-users.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const { Sequelize } = require("../seeders/models");
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('Users', 'phone_num', {
+      type: Sequelize.STRING,
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn('Users', 'phone_num');
+  },
+};
